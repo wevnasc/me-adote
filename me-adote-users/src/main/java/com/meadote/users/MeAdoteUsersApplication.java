e5b323e13a11a98487b137c83d9d165e26974d53
wnascimento <weverson.sn@gmail.com>
2017-03-03 14:00:34 -0300

    Configuração de micro serviços

---
+++ me-adote-users/src/main/java/com/meadote/users/MeAdoteUsersApplication.java
@@ -0,0 +1,14 @@
+package com.meadote.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MeAdoteUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeAdoteUsersApplication.class, args);
+	}
+}
