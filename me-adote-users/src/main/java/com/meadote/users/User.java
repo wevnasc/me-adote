c9de2ea6504efae95e8753168435fd3e6fc62bfc
wnascimento <weverson.sn@gmail.com>
2017-03-07 14:37:26 -0300

    Api Users Rest

---
+++ me-adote-users/src/main/java/com/meadote/users/User.java
@@ -0,0 +1,52 @@
+package com.meadote.users;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String email;
+    private String uri;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ me-adote-users/src/main/java/com/meadote/users/UserRepository.java
@@ -0,0 +1,14 @@
+package com.meadote.users;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    List<User> findByName(@Param("name") String name);
+
+}
