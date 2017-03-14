dec39cbb5ea77cde2dbe5563aaae6209538c1d3d
wnascimento <weverson.sn@gmail.com>
2017-03-14 16:59:21 -0300

    descoberta de servicos, configuração centralizada

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f5/b05b01feee0800171d37fd357c78e0d6
@@ -0,0 +1,12 @@
+package com.meadote.pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartPetsApplication.class, args);
+	}
+}
--- me-adote-pets/src/main/java/com/meadote/pets/StartPetsApplication.java
+++ me-adote-pets/src/main/java/com/meadote/pets/StartPetsApplication.java
@@ -2,8 +2,10 @@ package com.meadote.pets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StartPetsApplication {
 
 	public static void main(String[] args) {
